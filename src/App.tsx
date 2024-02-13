d2b404035bc37a7cc9f46df6fe5cb81f529bdafd
Андрей Романов <human1488pwnz@yandex.ru>
2024-02-13 12:36:47 +0300

    Router configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+import { Home, Project } from "./pages"
+
 const App = () => {
   return (
-    <></>
+    <Routes>
+      <Route path="/" /> {<Home />}
+      <Route path="/project" /> {<Project />}
+    </Routes>
   )
 }
 
