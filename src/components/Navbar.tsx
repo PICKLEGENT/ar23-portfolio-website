992f9b7475d68798c8f7d39bd72e3dade56a0847
Андрей Романов <human1488pwnz@yandex.ru>
2024-02-13 12:59:48 +0300

    Fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
     return (
         <>
-
+            <nav className="flex justify-around items-center text-primary-white">
+                <Link to='/'>about me</Link>
+                <Link to='/'>portfolio</Link>
+                <Link to='/'>contact</Link>
+            </nav>
         </>
     )
 }
