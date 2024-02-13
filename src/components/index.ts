dc4019008af61a49a49ff5a00258be69cb5aa423
Андрей Романов <human1488pwnz@yandex.ru>
2024-02-13 12:09:54 +0300

    Base

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Header from './Header';
+import Navbar from './Navbar';
+import CustomButton from './CustomButton';
+import CustomInput from './CustomInput';
+import Footer from './Footer';
+
+export { Header, Navbar, CustomButton, CustomInput, Footer };
