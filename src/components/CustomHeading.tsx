f921b33bf5c6ced8a2be75f927bcdc10718a8e5a
Андрей Романов <human1488pwnz@yandex.ru>
2024-02-21 11:57:03 +0300

    Little Update

--- src/components/CustomHeading.tsx
+++ src/components/CustomHeading.tsx
@@ -2,7 +2,7 @@ import { CustomHeadingProps } from "../types"
 
 const CustomHeading = ({ title }: CustomHeadingProps) => {
     return (
-        <p className="my-50 text-8xl font-bold text-primary-white tracking-widest">{title}</p>
+        <p className="my-50 text-7xl font-bold text-primary-white tracking-widest">{title}</p>
     )
 }
 
