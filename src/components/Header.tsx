397b7bff21d8db4cbc74695dc62b306caf417e97
Андрей Романов <human1488pwnz@yandex.ru>
2024-04-13 14:08:37 +0300

    Revert "Burger menu"
    This reverts commit aef09acec8fabd99688389b92c581434d892a542.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "."
 
 const Header = () => {
     return (
-        <header className="flex justify-between items-start lg:items-center">
+        <header className="flex justify-between items-center">
             <Link to='/' className="font-black text-4xl text-purple-800 dark:text-yellow-400">AR23</Link>
-            <div className="flex justify-center items-center p-3 lg:p-5 border-2 border-purple-800 dark:border-yellow-400 rounded-lg lg:rounded-full">
+            <div className="hidden lg:flex justify-center items-center p-5 border-2 border-purple-800 dark:border-yellow-400 rounded-full">
                 <Navbar />
             </div>
         </header>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     const { t } = useTranslation()
 
     return (
-        <nav className="flex flex-col lg:flex-row justify-center items-end lg:items-center font-bold text-2xl dark:text-neutral-100 gap-y-3 lg:gap-x-12">
+        <nav className="flex justify-center items-center font-bold text-2xl dark:text-neutral-100">
             <Link to='/about'>{t('title_2')}</Link>
-            <Link to='/portfolio'>{t('title_3')}</Link>
+            <Link to='/portfolio' className="mx-10">{t('title_3')}</Link>
             <Link to='/contact'>{t('title_4')}</Link>
         </nav>
     )
